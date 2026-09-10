3080cd817cda308e88766645a1a8634f6a72173a
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-10 14:49:08 +0200

    look v13 laden

--- pwa.js
+++ pwa.js
@@ -2,10 +2,10 @@
   if("serviceWorker" in navigator){
     navigator.serviceWorker.register("./sw.js").catch(function(){});
   }
-  var a=document.createElement("script");
-  a.src="ritual-list.js?v=1";
-  document.body.appendChild(a);
-  var b=document.createElement("script");
-  b.src="ritual-navkill.js?v=1";
-  document.body.appendChild(b);
+  var files=["ritual-list.js?v=1","ritual-navkill.js?v=1","ritual-look.js?v=13"];
+  files.forEach(function(src){
+    var s=document.createElement("script");
+    s.src=src;
+    document.body.appendChild(s);
+  });
 })();
