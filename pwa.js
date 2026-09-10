2301ae68736d75c13a18674f665e37db65ed3f3f
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-10 13:18:34 +0200

    pwa lädt Person-Tab

--- pwa.js
+++ pwa.js
@@ -2,4 +2,7 @@
   if("serviceWorker" in navigator){
     navigator.serviceWorker.register("./sw.js").catch(function(){});
   }
+  var s=document.createElement("script");
+  s.src="ritual-person-boot.js?v=1";
+  document.body.appendChild(s);
 })();
