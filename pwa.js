11736a35149ca33c2bebe534e7f0e0013885eb3b
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 11:49:22 +0200

    PWA Auflage 2, Cache leeren

--- pwa.js
+++ pwa.js
@@ -1,5 +1,12 @@
 (function(){
-  if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("./sw.js?v=4").catch(function(){});
+  if(!("serviceWorker" in navigator)) return;
+  if(window.caches){
+    caches.keys().then(function(keys){
+      keys.forEach(function(k){ caches.delete(k); });
+    });
   }
+  navigator.serviceWorker.getRegistrations().then(function(rs){
+    rs.forEach(function(r){ r.update(); });
+  });
+  navigator.serviceWorker.register("./sw.js?v=5").catch(function(){});
 })();
