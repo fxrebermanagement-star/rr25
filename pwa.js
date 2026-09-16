c9e25dd6e710c00cd96bd41d536f1729b4b33675
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 10:10:07 +0200

    trenn2.js laden

--- pwa.js
+++ pwa.js
@@ -39,6 +39,7 @@
     "ritual-liebe.js?v=2",
     "ritual-liebe2.js?v=1",
     "ritual-trenn.js?v=1",
+    "ritual-trenn2.js?v=1",
     "ritual-gate.js?v=15",
     "ritual-stabil.js?v=1",
     "ritual-entlass-fix.js?v=1",
