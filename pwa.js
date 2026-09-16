5a6e0a631886d7a0fd52543af7e0433f33483ca6
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 09:51:40 +0200

    heil.js laden

--- pwa.js
+++ pwa.js
@@ -34,6 +34,7 @@
     "ritual-stopp.js?v=3",
     "ritual-schutz2.js?v=2",
     "ritual-zur.js?v=1",
+    "ritual-heil.js?v=1",
     "ritual-gate.js?v=15",
     "ritual-stabil.js?v=1",
     "ritual-entlass-fix.js?v=1",
