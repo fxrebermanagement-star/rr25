4d4d0dd195422b11f235c86fee2582b1d693be88
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 10:25:13 +0200

    Sort mehrfach anwenden gegen alte Liste

--- ritual-sort.js
+++ ritual-sort.js
@@ -1,10 +1,16 @@
 (function(){
-  if(typeof R==="undefined") return;
-  for(var i=R.length-1;i>=0;i--){
-    var r=R[i];
-    if(r.id==="finst") R.splice(i,1);
-    else if(r.id==="schaden"){ r.tag="Person X"; r.t="Schadenszauber"; r.s="Vorhanden. Nicht Pflicht."; }
-    else if(r.id==="wesen"){ r.t="Wesenheit für Auftrag"; r.s="Kontakt mit Auftrag. Mass halten."; r.tag="Feld"; }
+  function go(){
+    if(typeof R==="undefined") return;
+    for(var i=R.length-1;i>=0;i--){
+      var r=R[i];
+      if(r.id==="finst") R.splice(i,1);
+      else if(r.id==="schaden"){ r.tag="Person X"; r.t="Schadenszauber"; r.s="Vorhanden. Nicht Pflicht."; }
+      else if(r.id==="wesen"){ r.t="Wesenheit für Auftrag"; r.s="Kontakt mit Auftrag. Mass halten."; r.tag="Feld"; }
+    }
+    if(typeof renderList==="function") renderList();
   }
-  if(typeof renderList==="function") renderList();
+  go();
+  setTimeout(go,200);
+  setTimeout(go,800);
+  setTimeout(go,1600);
 })();
