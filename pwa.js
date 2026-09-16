ecd046b2989149a64ce1cf22af854f02e47e8c15
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 10:24:23 +0200

    sort.js zuletzt

--- pwa.js
+++ pwa.js
@@ -46,7 +46,8 @@
     "ritual-stabil.js?v=1",
     "ritual-entlass-fix.js?v=1",
     "ritual-run-bind.js?v=1",
-    "ritual-nohak.js?v=2"
+    "ritual-nohak.js?v=2",
+    "ritual-sort.js?v=1"
   ];
   files.forEach(function(src){
     var s=document.createElement("script");
