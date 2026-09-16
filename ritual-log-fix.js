70eaf0f5b4a3bb38236daea9bab861a2f10182aa
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 18:58:06 +0200

    Chronik-Fotos nicht mehr entfernen

--- ritual-log-fix.js
+++ ritual-log-fix.js
@@ -1,14 +1,3 @@
 (function(){
   if(typeof enhanceLogFotos==="function") enhanceLogFotos=function(){};
-  var prev=typeof paintLog==="function"?paintLog:null;
-  paintLog=function(){
-    if(prev) prev();
-    var box=document.getElementById("entries");
-    if(!box) return;
-    box.querySelectorAll(".shots").forEach(function(s){ s.remove(); });
-    box.querySelectorAll(".logrow").forEach(function(row){
-      var pics=row.querySelectorAll("img");
-      for(var i=1;i<pics.length;i++) pics[i].remove();
-    });
-  };
 })();
