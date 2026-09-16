52d03077253b3d6452a62f833ece53843dcc0c75
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 10:28:21 +0200

    Fremd umbenennen: Ungebeten schliessen

--- ritual-sort.js
+++ ritual-sort.js
@@ -6,6 +6,7 @@
       if(r.id==="finst") R.splice(i,1);
       else if(r.id==="schaden"){ r.tag="Person X"; r.t="Schadenszauber"; r.s="Vorhanden. Nicht Pflicht."; }
       else if(r.id==="wesen"){ r.t="Wesenheit für Auftrag"; r.s="Kontakt mit Auftrag. Mass halten."; r.tag="Feld"; }
+      else if(r.id==="fremd"){ r.t="Ungebeten schliessen"; r.s="Kein Auftrag. Nur zu."; r.tag="Feld"; }
     }
     if(typeof renderList==="function") renderList();
   }
