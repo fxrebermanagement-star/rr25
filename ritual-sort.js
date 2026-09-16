2975929a00262097bf8865a0e5e9eb128f42c333
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 10:24:12 +0200

    Schadenszauber nach Person X, Wesen-Name, Festigen raus

---
+++ ritual-sort.js
@@ -0,0 +1,10 @@
+(function(){
+  if(typeof R==="undefined") return;
+  for(var i=R.length-1;i>=0;i--){
+    var r=R[i];
+    if(r.id==="finst") R.splice(i,1);
+    else if(r.id==="schaden"){ r.tag="Person X"; r.t="Schadenszauber"; r.s="Vorhanden. Nicht Pflicht."; }
+    else if(r.id==="wesen"){ r.t="Wesenheit für Auftrag"; r.s="Kontakt mit Auftrag. Mass halten."; r.tag="Feld"; }
+  }
+  if(typeof renderList==="function") renderList();
+})();
