81dc4166b3178a06c708e1fb67b6af8d354cb451
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-16 10:12:47 +0200

    nohak.js

--- pwa.js
+++ pwa.js
@@ -43,7 +43,8 @@
     "ritual-gate.js?v=15",
     "ritual-stabil.js?v=1",
     "ritual-entlass-fix.js?v=1",
-    "ritual-run-bind.js?v=1"
+    "ritual-run-bind.js?v=1",
+    "ritual-nohak.js?v=1"
   ];
   files.forEach(function(src){
     var s=document.createElement("script");
