ba5b420560e1398bc84484f211c6ac71a63e0d87
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-18 10:49:03 +0200

    Aufräumen: ungenutztes Skript weg

--- ritual-boot.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var s1=document.createElement("script"); s1.src="ritual-wesen-lead.js?v=4"; s1.async=false;
-  var s2=document.createElement("script"); s2.src="ritual-gate.js?v=13"; s2.async=false;
-  var s3=document.createElement("script"); s3.src="ritual-entlass-fix.js?v=1"; s3.async=false;
-  document.body.appendChild(s1);
-  document.body.appendChild(s2);
-  document.body.appendChild(s3);
-})();
