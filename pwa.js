9ae28e2d8f33a087ffe6dee239f8ca0b03b68cd7
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-22 15:37:18 +0200

    PWA Update erzwingen

--- pwa.js
+++ pwa.js
@@ -8,5 +8,15 @@
   navigator.serviceWorker.getRegistrations().then(function(rs){
     rs.forEach(function(r){ r.update(); });
   });
-  navigator.serviceWorker.register("./sw.js?v=0").catch(function(){});
+  navigator.serviceWorker.register("./sw.js?v=4").then(function(reg){
+    if(reg.waiting){
+      try{ reg.waiting.postMessage("skip"); }catch(e){}
+    }
+    setTimeout(function(){ try{ reg.update(); }catch(e){} }, 4000);
+  }).catch(function(){});
+  navigator.serviceWorker.addEventListener("controllerchange", function(){
+    if(window.__rr25reload) return;
+    window.__rr25reload=1;
+    location.reload();
+  });
 })();
