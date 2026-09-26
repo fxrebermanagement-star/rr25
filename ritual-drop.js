9179d4a79e8f12a90009f20d57b354c56d9ca8b3
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-26 22:33:47 +0200

    Aufräumen: ungenutztes ritual-drop.js entfernt (nicht geladen)

--- ritual-drop.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  ["fil","fremd"].forEach(function(id){
-    for(var i=R.length-1;i>=0;i--) if(R[i].id===id) R.splice(i,1);
-  });
-  if(typeof renderList==="function") renderList();
-})();
